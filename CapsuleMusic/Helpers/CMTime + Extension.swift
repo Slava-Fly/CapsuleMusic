c509890af810db5649e8461716218425ab396105
kornienko_v <kornienko.v@orionnet.ru>
2025-10-16 02:01:57 +0800

    Mini Player View & Track transition animation and maximazing animation

---
+++ CapsuleMusic/Helpers/CMTime + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  CMTime + Extension.swift
+//  CapsuleMusic
+//
+//  Created by Славка Корн on 13.10.2025.
+//
+
+import Foundation
---
+++ CapsuleMusic/Helpers/Nib.swift
@@ -0,0 +1,8 @@
+//
+//  Nib.swift
+//  CapsuleMusic
+//
+//  Created by Славка Корн on 15.10.2025.
+//
+
+import Foundation
